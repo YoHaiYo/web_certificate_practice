a157796d6d24948aa503a021810ddc632f47aed2
YoHaiYo <12si47bun@naver.com>
2024-03-28 23:37:50 +0900

    B2 연습

---
+++ Practice/B-2/js/main.js
@@ -0,0 +1,26 @@
+$("document").ready(function(){
+
+  function horizontalSilde(){
+    $(".swiper-wrapper").animate({"marginLeft": -1200}, 300, function(){
+      $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
+      $(".swiper-wrapper").css({"marginLeft": 0})
+    })
+  }
+
+  function verticalSilde(){
+    $(".swiper-wrapper").animate({"marginTop": -300}, 300, function(){
+      $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
+      $(".swiper-wrapper").css({"marginTop": 0})
+    })
+  }
+  
+  let sildeIndex = 0;
+  let slideNum = $(".swiper-slide").length
+  function fadeSlide(){
+    sildeIndex++
+    sildeIndex = sildeIndex % slideNum
+    $(".swiper-slide").eq(sildeIndex).addClass("on").siblings().removeClass("on")
+  }
+
+  // setInterval(verticalSilde, 3000)
+})
